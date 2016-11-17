ec8421daab99102158643b9780a30025d6751283
github username <github email address>
2016-11-17 05:35:11 -0500

    Done.

---
+++ components/RegistrationForm.js
@@ -0,0 +1,15 @@
+const React = require('react');
+
+class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form action="#" method="POST">
+        <input type="text"></input>
+        <input type="password"></input>
+        <button type="submit"></button>
+      </form>
+    );
+  }
+}
+
+module.exports = RegistrationForm;
